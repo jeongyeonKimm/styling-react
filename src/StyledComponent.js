e3bb74eda501f40fad7fec5b77d2acf4ca462020
kim_jeongyeon <wjddus9132@gmail.com>
2022-01-31 20:44:48 +0900

    styled components sample01

--- src/StyledComponent.js
+++ src/StyledComponent.js
@@ -7,35 +7,36 @@ const Box = styled.div`
 `;
 
 const Button = styled.button`
-  background: white;
-  color: black;
-  border-radius: 4px;
-  padding: 0.5rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  box-sizing: border-box;
-  font-size: 1rem;
-  font-weight: 600;
+    background: white;
+    color: black;
+    border-radius: 4px;
+    padding: 0.5rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-sizing: border-box;
+    font-size: 1rem;
+    font-weight: 600
 
-  &:hover {
-    background: rgba(255, 255, 255, 0.9);
-  }
+    &:hover {
+        background: rgba(255, 255, 255, 0.9);
+    }
 
-  ${(props) =>
-    props.inverted &&
-    css`
-      background: none;
-      border: 2px solid white;
-      color: white;
-      &:hover {
-        background: white;
-        color: black;
-      }
-    `};
-  & + button {
-    margin-left: 1rem;
-  }
+    ${(props) =>
+      props.inverted &&
+      css`
+        background: none;
+        border: 2px solid white;
+        color: white;
+
+        &:hover {
+          background: white;
+          color: black;
+        }
+      `};
+    & + button {
+        margin-left: 1rem;
+    }
 `;
 
 const StyledComponent = () => {
